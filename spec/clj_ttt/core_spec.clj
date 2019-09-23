38923a7b192c0dd96897ef2525327f146dfa572b
crkubiak <charles@8thlight.com>
2019-09-23 14:08:29 -0500

    initial commit

---
+++ spec/clj_ttt/core_spec.clj
@@ -0,0 +1,42 @@
+(ns clj-ttt.core-spec
+  (:require [speclj.core :refer :all]
+            [clj-ttt.core :refer :all]))
+
+(describe "A new game of tic-tac-toe begins with:"
+          (it "an empty board labeled 1-9"
+              (should= "1 2 3\n4 5 6\n7 8 9\n" (with-out-str(print-board starting-board))))
+
+          ;(it "with a current player X"
+          ;    (should= "X" player-sequence))
+          )
+
+(describe "As a game is played:"
+          ;(it "the current player switches at the end of a valid turn"
+          ;    ())
+          ;(it "a board with empty cells can be marked"
+          ;    ())
+          ;(it "a board cannot be marked in a filled cell"
+          ;    ())
+          ;(it "a player moves by choosing a cell location"
+          ;    ())
+          )
+
+(describe "a game of tic-tac-toe ends when:"
+          (it "a board is full"
+              (should (full-board? [:X :O :X :X :O :O :O :X :X])))
+          (it "a game is a draw if board is full"
+              (should= "current board:\nX O X\nX O O\nO X X\nGame is a draw.\n" (with-out-str(play-game [:X :O
+              :X :X :O :O :O :X :X] player-sequence))))
+
+          ;(it "has a winning combination"
+          ;    (should= ((:X :X :X) (:O :O 6) (7 8 9) (:X :O 7) (:X :O 8) (:X 6 9) (:X :O 9) (:X :O 7))
+          ;             (winning-boards [:X :X :X :O :O 6 7 8 9])))
+
+          (it "has a message that declares the winner X"
+              (should= "current board:\nX X X\nO O 6\n7 8 9\nPlayer X wins!\n" (with-out-str(play-game [:X :X :X :O :O 6 7 8 9] player-sequence))))
+
+          (it "has a message that declares the winner O"
+              (should= "current board:\n1 X X\nO O O\n7 8 9\nPlayer O wins!\n" (with-out-str(play-game [1 :X :X :O
+                                                                                                        :O :O 7 8 9]
+                                                                                                       player-sequence))))
+          )
---
+++ src/clj_ttt/core.clj
@@ -0,0 +1,79 @@
+(ns clj-ttt.core)
+
+(defn check-for-winner? [winning-combination]
+  (if (every? #{:X} winning-combination) :X
+                            (if (every? #{:O} winning-combination) :O)))
+
+(declare winning-boards)
+
+(defn winner? [board]
+  (first (filter #{:X :O} (map check-for-winner? (winning-boards board)))))
+
+(defn winning-boards [board]
+  (concat
+    (partition-all 3 board)
+    (list
+      (take-nth 3 board)
+      (take-nth 3 (drop 1 board))
+      (take-nth 3 (drop 2 board))
+      (take-nth 4 board)
+      (take-nth 2(drop-last 2 (drop 2 board))))))
+
+(defn full-board? [board]
+  (every? #{:X :O} board))
+
+(defn print-board [board]
+  (let [board (map #(if (keyword? %) (subs (str %) 1) %) board)]
+    (println (nth board 0) (nth board 1) (nth board 2))
+    (println (nth board 3) (nth board 4) (nth board 5))
+    (println (nth board 6) (nth board 7) (nth board 8))))
+
+(defn player-name [player]
+  (subs (str player) 1))
+
+(def starting-board [1 2 3 4 5 6 7 8 9])
+
+(def player-sequence ([:X :O]))
+
+(defn get-move [board]
+  (let [input (try
+                (. Integer parseInt (read-line))
+                (catch Exception e nil))]
+    (if (some #{input} board)
+      input
+      nil)))
+
+(defn take-turn [player board]
+  (println "Select your move, player" (player-name player) " (press 1-9 and hit enter):")
+  (loop [move (get-move board)]
+    (if move
+      (assoc board (dec move) player)
+      (do
+        (println "Move was invalid. Select your move, player" (str (player-name player) ":"))
+        (recur (get-move board))))))
+
+(defn play-game
+  ([] (play-game starting-board player-sequence))
+  ([board player-sequence]
+
+    (let [winner (winner? board)]
+      (println "current board:")
+      (print-board board)
+      (cond
+        winner (println "Player" (player-name winner) "wins!")
+        (full-board? board) (println "Game is a draw.")
+        :else
+        (recur
+          (take-turn (first player-sequence) board)
+          (rest player-sequence))))))
+
+;sep io from decision making in play-game
+
+;(play-game)
+;(println (full-board? [:X :O :X :X :O :O :O :X :X]))
+;(println (winning-boards [:X :X :X :O :O 6 7 8 9]))
+(println (winner? [:X :X :X :O :O 6 7 8 9]))
+
+(println (winner? [1 :X :X :O :O :O 7 8 9]))
+
+(println (player-name ))
